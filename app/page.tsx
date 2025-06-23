befa3bfadf7828a822f07c7739d8ac111c269570
Laiyuhung <laiyuhung921118@gmail.com>
2025-06-23 22:06:15 +0800

    0623

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>歡迎來到我的網站！</h1>
-      <p>這是用 Next.js 架設並部署到 Vercel 的簡單網站。</p>
+      <p>這是用 Next.js 架設並部署到 Vercel 的簡單網站。test</p>
     </main>
   )
 }
